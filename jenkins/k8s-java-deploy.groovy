df7f28bde4403002981c5455f12c4c2617974089
abdulmelikkalkan <kalkanabdulmelik@gmail.com>
2023-05-27 13:34:37 +0300

    Add jenkins folder

---
+++ jenkins/k8s-java-deploy.groovy
@@ -0,0 +1,35 @@
+#!/usr/bin/env groovy
+
+// @Library('getir-jenkins-library')_
+
+
+pipeline {
+    agent any
+
+    tools {
+    }
+
+    stages {
+        stage('Prepare & Checkout') {
+            steps {
+                script {
+                    // env.JENKINS_OPERATION="pipeline-java-image-deploy-ocp-prod"
+                }
+                // stagePrepare()
+            }
+            post {
+                success {
+                    script {
+                        announce("Stage success")
+                    }
+                }
+                failure {
+                    script {
+                        announce("Pipeline failure")
+                        // parametersLogging()
+                    }
+                }
+            }
+        }
+    }
+}
