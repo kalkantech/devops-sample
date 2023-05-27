a843d9f53f715ce74bf5d3af8274e860f9997917
abdulmelikkalkan <kalkanabdulmelik@gmail.com>
2023-05-27 19:52:24 +0300

    checkout app

--- jenkins/vars/checkoutApp.groovy
+++ jenkins/vars/checkoutApp.groovy
@@ -5,21 +5,22 @@ import org.apache.commons.lang.StringUtils;
 def call() {
     announce("Checkout projet git repository")
 
-    // sh "mkdir -p ${JENKINS_APP_DIR}"
-    // dir("${JENKINS_APP_DIR}") {
-    //     def scmVars = checkout([$class: 'GitSCM',
-    //     branches: [[name: "*/${JENKINS_SCM_REPO_BRANCH}"]],
-    //     userRemoteConfigs: [[credentialsId: 'bitbucket-git-credential-sa', url: "${JENKINS_SCM_REPO_URL}"]]])
+    sh "mkdir -p ${JENKINS_APP_DIR}"
+    dir("${JENKINS_APP_DIR}") {
+        def scmVars = checkout([$class: 'GitSCM',
+        branches: [[name: "*/${REPO_BRANCH}"]],
+        userRemoteConfigs: [[url: "${REPO_URL}"]]])
 
-    //     env.JENKINS_APP_GIT_BRANCH = scmVars.GIT_BRANCH
-    //     env.JENKINS_APP_GIT_COMMIT = scmVars.GIT_COMMIT
-    //     env.JENKINS_APP_GIT_PREVIOUS_COMMIT = scmVars.GIT_PREVIOUS_COMMIT
-    //     env.JENKINS_APP_GIT_PREVIOUS_SUCCESSFUL_COMMIT = scmVars.GIT_PREVIOUS_SUCCESSFUL_COMMIT
-    //     env.JENKINS_APP_GIT_URL = scmVars.GIT_URL
-    //     env.JENKINS_APP_GIT_COMMITTER_EMAIL = scmVars.GIT_COMMITTER_EMAIL
-    //     env.JENKINS_APP_GIT_AUTHOR_EMAIL = scmVars.GIT_AUTHOR_EMAIL
-    //     env.JENKINS_APP_GIT_COMMITTER_NAME = scmVars.GIT_COMMITTER_NAME
-    //     env.JENKINS_APP_GIT_AUTHOR_NAME = scmVars.GIT_AUTHOR_NAME
-    //     env.JENKINS_APP_GIT_REPO_NAME = StringUtils.substringBetween(scmVars.GIT_URL, "/", ".git")
-    // }
+        env.JENKINS_APP_GIT_BRANCH = scmVars.GIT_BRANCH
+        env.JENKINS_APP_GIT_COMMIT = scmVars.GIT_COMMIT
+        env.JENKINS_APP_GIT_PREVIOUS_COMMIT = scmVars.GIT_PREVIOUS_COMMIT
+        env.JENKINS_APP_GIT_PREVIOUS_SUCCESSFUL_COMMIT = scmVars.GIT_PREVIOUS_SUCCESSFUL_COMMIT
+        env.JENKINS_APP_GIT_URL = scmVars.GIT_URL
+        env.JENKINS_APP_GIT_COMMITTER_EMAIL = scmVars.GIT_COMMITTER_EMAIL
+        env.JENKINS_APP_GIT_AUTHOR_EMAIL = scmVars.GIT_AUTHOR_EMAIL
+        env.JENKINS_APP_GIT_COMMITTER_NAME = scmVars.GIT_COMMITTER_NAME
+        env.JENKINS_APP_GIT_AUTHOR_NAME = scmVars.GIT_AUTHOR_NAME
+        env.JENKINS_APP_GIT_REPO_NAME = StringUtils.substringBetween(scmVars.GIT_URL, "/", ".git")
+        env.JENKINS_APP_GIT_REPO_NAME = scmVars.GIT_URL.substring(scmVars.GIT_URL.lastIndexOf('/') + 1, scmVars.GIT_URL.lastIndexOf('.git'))
+    }
 }
--- jenkins/vars/parametersStatic.groovy
+++ jenkins/vars/parametersStatic.groovy
@@ -5,6 +5,7 @@ def call() {
     announce("Preparing static parameters")
     
     // env.JENKINS_APP_DIR=StringUtils.substringBetween(JENKINS_SCM_REPO_URL, "/", ".git")
+    env.JENKINS_APP_DIR=REPO_URL.substring(REPO_URL.lastIndexOf('/') + 1, REPO_URL.lastIndexOf('.git'))
     // withCredentials([usernamePassword(credentialsId: "bitbucket-git-credential-sa-with-app-password", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]){
     //     env.JENKINS_BITBUCKET_SA_USER="${USERNAME}"
     //     env.JENKINS_BITBUCKET_SA_USER_PASSWORD="${PASSWORD}"
